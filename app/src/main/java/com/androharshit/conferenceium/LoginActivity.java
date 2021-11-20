efc77568587712924934c538d35e1a29d33f002d
Harshit Verma <harshitverma14366@gmail.com>
2021-11-20 15:48:22 +0530

    new

--- app/src/main/java/com/androharshit/conferenceium/LoginActivity.java
+++ app/src/main/java/com/androharshit/conferenceium/LoginActivity.java
@@ -2,17 +2,29 @@ package com.androharshit.conferenceium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide(); //hide the toolBar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // hide statusBar
-
+        init();
+        actions();
+    }
+    private void init(){
+        signup=findViewById(R.id.signup_btn);
+    }
+    private void actions(){
+        signup.setOnClickListener(v->{
+            Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/androharshit/conferenceium/MainActivity.java
+++ app/src/main/java/com/androharshit/conferenceium/MainActivity.java
@@ -2,7 +2,9 @@ package com.androharshit.conferenceium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide(); //hide the toolBar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // hide statusBar
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        },5000);
     }
 }
