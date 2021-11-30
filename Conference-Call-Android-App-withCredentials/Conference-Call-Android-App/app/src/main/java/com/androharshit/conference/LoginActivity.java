3278a198e1b5a920cddaf56f7331485b4d7b6d4e
Harshit Verma <harshitverma14366@gmail.com>
2021-11-30 21:10:51 +0530

    with Credential

---
+++ Conference-Call-Android-App-withCredentials/Conference-Call-Android-App/app/src/main/java/com/androharshit/conference/LoginActivity.java
@@ -0,0 +1,129 @@
+package com.androharshit.conference;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText emailBox, passwordBox;
+    Button loginBtn, signupBtn;
+
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        auth =FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.setMessage("Just a moment...");
+
+        emailBox = findViewById(R.id.signup_email_edt);
+        passwordBox = findViewById(R.id.signup_password_edt);
+
+        loginBtn = findViewById(R.id.signup_back);
+        signupBtn = findViewById(R.id.goto_signup_page_btn);
+
+        loginBtn.setOnClickListener(v -> {
+            loginUser();
+
+        });
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+    }
+
+    private void loginUser()
+    {
+        emailBox.setError(null);
+        passwordBox.setError(null);
+        String email=emailBox.getText().toString();
+        String passwd=passwordBox.getText().toString();
+        boolean cancel=false;
+        View focusView=null;
+        if (TextUtils.isEmpty(passwd) && !checkPassword(passwd)) {
+            passwordBox.setError(getString(R.string.invalid_pass));
+            focusView=passwordBox;
+            cancel=true;
+        }
+        if (TextUtils.isEmpty(email) && !checkEmail(email)) {
+            emailBox.setError(getString(R.string.invalid_Email));
+            focusView=emailBox;
+            cancel=true;
+        }
+        if(cancel)
+        {
+            focusView.requestFocus();
+        }
+        else {
+            signinUser();
+        }
+    }
+
+    private void signinUser() {
+        progressDialog.show();
+
+        String email,password;
+        email=emailBox.getText().toString();
+        password=passwordBox.getText().toString();
+
+        auth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+
+                } else {
+                    Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private boolean checkEmail(String email)
+    {
+        return email.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+    }
+    private boolean checkPassword(String passwd)
+    {
+        return passwd.length()>6;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+        }
+    }
+}
---
+++ Conference-Call-Android-App-withCredentials/Conference-Call-Android-App/app/src/main/java/com/androharshit/conference/MainActivity.java
@@ -0,0 +1,98 @@
+package com.androharshit.conference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button back_Button,shareBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        back_Button = (Button) findViewById(R.id.LogOutButton);
+        back_Button.setOnClickListener(v1 -> {
+            destroye_it();
+        });
+        try {
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(new URL(""))
+                    .setWelcomePageEnabled(false)
+                    .setFeatureFlag("invite.enabled",false)
+                    .setFeatureFlag("help.enabled",false)
+                    .build();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        shareBtn=findViewById(R.id.share);
+        shareBtn.setOnClickListener(view -> {
+            boolean cancel=false;
+            View focusView=null;
+            EditText editText= findViewById(R.id.codeBox);
+            editText.setError(null);
+            if (editText.getText().toString().length() <1) {
+                editText.setError(getString(R.string.invalid_share_text));
+                focusView=editText;
+                cancel=true;
+            }
+            if(cancel)
+            {
+                focusView.requestFocus();
+            }
+            else
+            {
+                String strCode="Hi dear! let's catch up meet, to join the meeting on Conferenceium Android App by my room code : \n" +"'"+editText.getText().toString()+"'\n"+ "and download the app by attached link"+ "\n"+"https://play.google.com/store/apps/details?id=com.androharshit.conference" ;
+                Intent intent=new Intent();
+                intent.setAction(intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT,strCode);
+                intent.setType("text/plain");
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void destroye_it() {
+        finish();
+    }
+
+
+    public void onButtonClick(View v) {
+        EditText editText = findViewById(R.id.codeBox);
+        String text = editText.getText().toString();
+        if (text.length() > 0) {
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .setFeatureFlag("invite.enabled",false)
+                    .setFeatureFlag("help.enabled",false)
+                    .build();
+            JitsiMeetActivity.launch(this, options);
+        }
+
+        // This is a demo app  You can add share button Functionality
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "App Closed!", Toast.LENGTH_SHORT).show();
+    }
+}
